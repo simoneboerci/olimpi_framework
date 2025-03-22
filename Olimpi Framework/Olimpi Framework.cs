9e651435d4005508a13553ec669ebe2d92778710
Simone Boerci <boercisimone@gmail.com>
2025-03-22 11:35:41 +0100

    Default cAlgo project

---
+++ Olimpi Framework/Olimpi Framework.cs
@@ -0,0 +1,33 @@
+using System;
+using cAlgo.API;
+using cAlgo.API.Collections;
+using cAlgo.API.Indicators;
+using cAlgo.API.Internals;
+
+namespace cAlgo.Robots
+{
+    [Robot(AccessRights = AccessRights.None, AddIndicators = true)]
+    public class OlimpiFramework : Robot
+    {
+        [Parameter(DefaultValue = "Hello world!")]
+        public string Message { get; set; }
+
+        protected override void OnStart()
+        {
+            // To learn more about cTrader Automate visit our Help Center:
+            // https://help.ctrader.com/ctrader-automate
+
+            Print(Message);
+        }
+
+        protected override void OnTick()
+        {
+            // Handle price updates here
+        }
+
+        protected override void OnStop()
+        {
+            // Handle cBot stop here
+        }
+    }
+}
